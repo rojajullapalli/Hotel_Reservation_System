b8976f7199c7f7029057987cc20974c154de059b
rojajullapalli <rojajules@gmail.com>
2021-09-11 08:36:10 +0530

    MODIFIED: Regular Customer Rates

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -2,27 +2,9 @@ public class Hotel {
     private String hotelName;
     private int regularCustomerRate;
 
-    // parameterized constructor
     public Hotel(String hotelName, int regularCustomerRate) {
         this.hotelName = hotelName;
         this.regularCustomerRate = regularCustomerRate;
-    }
-
-    // getters and setters
-    public String getHotelName() {
-        return hotelName;
-    }
-
-    public void setHotelName(String hotelName) {
-        this.hotelName = hotelName;
-    }
-
-    public int getRegularCustomerRate() {
-        return regularCustomerRate;
-    }
-
-    public void setRegularCustomerRate(int rate) {
-        this.regularCustomerRate = rate;
     }
 
 }
--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -7,6 +7,8 @@ public class HotelReservationTest {
         System.out.println("Welcome to Hotel Reservation System");
         HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
         Hotel hotelCheck = new Hotel("Bridgewood", 160);
+        Hotel hotelCheck1 = new Hotel("Lakewood", 110);
+        Hotel hotelCheck2 = new Hotel("Ridgewood", 220);
         try{
             boolean checkIfAdded = hotelReservationSystem.addHotel(hotelCheck);
             Assertions.assertEquals(checkIfAdded, true);
