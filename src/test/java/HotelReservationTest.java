b390e03d5ff4a24091b9391553a7d990e08bb4f3
rojajullapalli <rojajules@gmail.com>
2021-09-11 08:22:24 +0530

    ADDED: Welcome Message

---
+++ src/test/java/HotelReservationTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class HotelReservationTest {
+    @Test
+    public void toAddHotelHotel_Should_returnTrue() {
+        System.out.println("welcome to Hotel Reservation System");
+    }
+}
