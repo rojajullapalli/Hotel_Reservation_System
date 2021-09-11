1fcea81e792b3e1cd837a1c348476b715fe96a25
rojajullapalli <rojajules@gmail.com>
2021-09-11 08:46:47 +0530

    MODIFIED: Regular Customer Rates

--- src/main/java/HotelReservationSystem.java
+++ src/main/java/HotelReservationSystem.java
@@ -4,7 +4,9 @@ public class HotelReservationSystem {
     private ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
 
     public boolean addHotel(Hotel newHotel) {
+        System.out.println("");
         hotelList.add(newHotel);
         return true;
     }
+
 }
